f726c889dfc8a4aea27a35ab09825a33da03ec49
Lê Ngân <prohotvulcan@outlook.com>
2020-03-15 23:03:26 +0700

    commit service

---
+++ olShopSolution/olShop.Application/Interfaces/IFunctionService.cs
@@ -0,0 +1,29 @@
+﻿using olShop.Application.ViewModels.System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace olShop.Application.Interfaces
+{
+    public interface IFunctionService
+    {
+        void Add(FunctionViewModel functionVm);
+
+        void Update(FunctionViewModel functionVm);
+
+        void Delete(string id);
+
+        Task<List<FunctionViewModel>> GetAll(string filter);
+
+        IEnumerable<FunctionViewModel> GetAllWithParentId(string parentId);
+
+        FunctionViewModel GetById(string id);
+
+        bool CheckExistedId(string id);
+
+        void UpdateParentId(string sourceId, string targetId, Dictionary<string, int> items);
+
+        void ReOrder(string sourceId, string targetId);
+
+        void Save();
+    }
+}
---
+++ olShopSolution/olShop.Application/Interfaces/IPageService.cs
@@ -0,0 +1,25 @@
+﻿using olShop.Application.ViewModels.Blog;
+using olShop.Utilities.Dtos;
+using System.Collections.Generic;
+
+namespace olShop.Application.Interfaces
+{
+    public interface IPageService
+    {
+        void Add(PageViewModel pageVm);
+
+        void Update(PageViewModel pageVm);
+
+        void Delete(int id);
+
+        List<PageViewModel> GetAll();
+
+        PagedResult<PageViewModel> GetAllPaging(string keyword, int page, int pageSize);
+
+        PageViewModel GetByAlias(string alias);
+
+        PageViewModel GetById(int id);
+
+        void Save();
+    }
+}
