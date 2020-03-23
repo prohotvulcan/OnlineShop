e7662ca66ce8948adb19f90aa84a2aee28e8820e
Lê Ngân <prohotvulcan@outlook.com>
2020-03-23 17:56:22 +0700

    commited

--- olShopSolution/olShop.WebApp/Controllers/HomeController.cs
+++ olShopSolution/olShop.WebApp/Controllers/HomeController.cs
@@ -3,24 +3,51 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
+using olShop.Application.Interfaces;
 using olShop.WebApp.Models;
 
 namespace olShop.WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
+        private readonly IProductCategoryService _productCategoryService;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IBlogService _blogService;
+        private readonly ICommonService _commonService;
+        private readonly IStringLocalizer<HomeController> _localizer;
+
+        public HomeController(IProductService productService,
+            IProductCategoryService productCategoryService,
+            IBlogService blogService,
+            ICommonService commonService,
+            IStringLocalizer<HomeController> localizer)
         {
-            _logger = logger;
+            this._productService = productService;
+            this._productCategoryService = productCategoryService;
+            this._blogService = blogService;
+            this._commonService = commonService;
+            this._localizer = localizer;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var title = _localizer["Title"];
+            var culture = HttpContext.Features.Get<IRequestCultureFeature>().RequestCulture.Culture.Name;
+            ViewData["BodyClass"] = "cms-index-index cms-home-page";
+            var homeVm = new HomeViewModel
+            {
+                HomeCategories = _productCategoryService.GetHomeCategories(5),
+                HotProducts = _productService.GetHotProduct(5),
+                TopSellProducts = _productService.GetLastest(5),
+                LatestBlogs = _blogService.GetLastest(5),
+                HomeSlides = _commonService.GetSlides("top")
+            };
+            return View(homeVm);
         }
 
         public IActionResult Privacy()
@@ -32,6 +59,15 @@ namespace olShop.WebApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new Microsoft.AspNetCore.Http.CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) });
+            return LocalRedirect(returnUrl);
         }
     }
 }
---
+++ olShopSolution/olShop.WebApp/Models/HomeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using olShop.Application.ViewModels.Blog;
+using olShop.Application.ViewModels.Common;
+using olShop.Application.ViewModels.Product;
+using System.Collections.Generic;
+
+namespace olShop.WebApp.Models
+{
+    public class HomeViewModel
+    {
+        public List<BlogViewModel> LatestBlogs { get; set; }
+        public List<SlideViewModel> HomeSlides { get; set; }
+        public List<ProductViewModel> HotProducts { get; set; }
+        public List<ProductViewModel> TopSellProducts { get; set; }
+        public List<ProductCategoryViewModel> HomeCategories { get; set; }
+
+        public string Title { get; set; }
+        public string MetaKeyword { get; set; }
+        public string MetaDescription { get; set; }
+    }
+}
